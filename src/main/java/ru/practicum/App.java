b9c574e053f5ae044ebdcec22feece7e4f51f22b
Matvey <matvey.oleynik.2017@mail.ru>
2025-06-25 16:50:24 +0300

    доделать дома?

--- src/main/java/ru/practicum/App.java
+++ src/main/java/ru/practicum/App.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Hello world!
@@ -15,15 +12,29 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         String fileName = "lng.txt";
+        List<String[]> splitLines = readFileAndValid(fileName);
+        for (String[] splitLine : splitLines) {
+            System.out.println(Arrays.toString(splitLine));
+        }
+
+    }
+
+    public static List<String[]> readFileAndValid(String fileName){
+        List<String[]> lines = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while ((line = br.readLine()) != null) {
 
+            while ((line = br.readLine()) != null) {
+                if (isValidLine(line)){
+                    lines.add(line.split(";"));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return lines;
     }
 
     public static boolean isValidLine(String line) {
